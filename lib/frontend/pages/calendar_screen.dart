223a511bdce10f2dfb90190306bc90cca210562b
Thivishan <thivishanthevarasa25@gmail.com>
2026-03-07 17:18:11 +0530

    Added calendar screen and connected to homepage

---
+++ lib/frontend/pages/calendar_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CalendarScreen extends StatelessWidget {
+  const CalendarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calendar"),
+        backgroundColor: const Color(0xFF172B35),
+      ),
+      body: const Center(
+        child: Text("Calendar Screen", style: TextStyle(fontSize: 22)),
+      ),
+    );
+  }
+}
