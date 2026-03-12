2e15aaf98d9acca2d3ad92527807f7c5085f2903
Titan7k <titan7kzc@gmail.com>
2026-03-13 02:38:05 +0530

    profile page skeleton

--- lib/frontend/main_screens/profile_screen.dart
+++ lib/frontend/main_screens/profile_screen.dart
@@ -5,8 +5,9 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Profile Screen", style: TextStyle(fontSize: 24)),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Profile')),
+      body: const Center(child: Text('Profile page')),
     );
   }
 }
